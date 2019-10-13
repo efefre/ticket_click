a2af7add7260b292548a751289663034be8781f6
Kasia <efefre@icloud.com>
2019-10-13 19:50:57 +0200

    Add app desription for --help

--- ticket_click/core.py
+++ ticket_click/core.py
@@ -9,6 +9,9 @@ from ticket_click.utils import Ticket
 @click.option('--period', default=30, prompt='Na ile dni wykupiłeś bilet (30/90)? ', help="Number: 30 or 90. Default value: 30")
 @click.option('--ticket_price',default=0.00, prompt='Ile zapłaciłeś za bilet? ', help="Ticker price: float. Default value: 0.00")
 def cli(start_date, stop_date, cancel_date, period, ticket_price):
+    """ This is a simple app in which you can count how much money you will receive back,
+        if you cancel your ZTM (Warsaw) ticket."""
+
     click.echo("""
     ******************************************************************************************************
     Jeżeli nie pamiętasz daty aktywacji biletu, to podaj datę końca ważności biletu
