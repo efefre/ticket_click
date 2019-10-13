0691674f8c6c512ab63b9b28c24e43c7ff45d2cd
Kasia <efefre@icloud.com>
2019-10-13 19:50:12 +0200

    Add click options: start_date, stop_date, cancel_date, period, ticket_price

--- ticket_click/core.py
+++ ticket_click/core.py
@@ -3,7 +3,12 @@ from ticket_click.utils import Ticket
 
 
 @click.command()
-def cli():
+@click.option('--start_date', prompt='Kiedy bilet został aktywowany? ', help="Date in format dd-mm-yyyy")
+@click.option('--stop_date', prompt='Do kiedy bilet jest ważny? ', help="Date in format dd-mm-yyyy")
+@click.option('--cancel_date', prompt='Kiedy chcesz anulować bilet? ', help="Date in format dd-mm-yyyy")
+@click.option('--period', default=30, prompt='Na ile dni wykupiłeś bilet (30/90)? ', help="Number: 30 or 90. Default value: 30")
+@click.option('--ticket_price',default=0.00, prompt='Ile zapłaciłeś za bilet? ', help="Ticker price: float. Default value: 0.00")
+def cli(start_date, stop_date, cancel_date, period, ticket_price):
     click.echo("""
     ******************************************************************************************************
     Jeżeli nie pamiętasz daty aktywacji biletu, to podaj datę końca ważności biletu
