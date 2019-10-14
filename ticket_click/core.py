ba8459f703b0422502759e212d56652efbb7dc66
Kasia <efefre@icloud.com>
2019-10-14 17:18:34 +0200

    Update option: start_date. Add default value '' (empty)

--- ticket_click/core.py
+++ ticket_click/core.py
@@ -1,9 +1,11 @@
 import click
+import pendulum
+
 from ticket_click.utils import Ticket
 
 
 @click.command()
-@click.option('--start_date', prompt='Kiedy bilet został aktywowany? ', help="Date in format dd-mm-yyyy")
+@click.option('--start_date', prompt='Kiedy bilet został aktywowany? ', help="Date in format dd-mm-yyyy", default='')
 @click.option('--stop_date', prompt='Do kiedy bilet jest ważny? ', help="Date in format dd-mm-yyyy")
 @click.option('--cancel_date', prompt='Kiedy chcesz anulować bilet? ', help="Date in format dd-mm-yyyy")
 @click.option('--period', default=30, prompt='Na ile dni wykupiłeś bilet (30/90)? ', help="Number: 30 or 90. Default value: 30")
